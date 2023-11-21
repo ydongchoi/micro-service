df17d9ebd745a13ccc030aeade83800940bae96b
YD <105weapon@gmail.com>
2023-11-21 23:44:35 +0900

    feat: added Prodcut Service

---
+++ src/main/java/com/ydong/productservice/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.ydong.productservice.service;
+
+import com.ydong.productservice.dto.ProductRequest;
+import com.ydong.productservice.model.Product;
+import com.ydong.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+
+}
