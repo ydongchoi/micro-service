93c157024ee65b1df3e0eef4d87c24882266cf53
YD <105weapon@gmail.com>
2023-11-21 23:45:59 +0900

    feat: added createProduct method

--- src/main/java/com/ydong/productservice/controller/ProductController.java
+++ src/main/java/com/ydong/productservice/controller/ProductController.java
@@ -1,16 +1,21 @@
 package com.ydong.productservice.controller;
 
 import com.ydong.productservice.dto.ProductRequest;
+import com.ydong.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/prooduct")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
-        
+        productService.createProduct(productRequest);
     }
 }
