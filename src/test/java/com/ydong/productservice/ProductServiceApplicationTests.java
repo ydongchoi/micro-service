4f1758654f29a73e17901a46ffda907e69e8219c
YD <105weapon@gmail.com>
2023-11-23 01:28:14 +0900

    feat: added mock mvc for integration test

--- src/test/java/com/ydong/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/ydong/productservice/ProductServiceApplicationTests.java
@@ -1,19 +1,25 @@
 package com.ydong.productservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
 	@Container
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.3");
+	@Autowired
+	private MockMvc mockMvc;
 
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
@@ -21,7 +27,8 @@ class ProductServiceApplicationTests {
 	}
 
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+
 	}
 
 }
